d0fb542e401025d753b78d81064774151d3962ce
DigitalSecurityDev <suruchi.srivastava@accenture.com>
2016-06-06 16:10:52 +0530

    Update wrapper.php

--- wrapper.php
+++ wrapper.php
@@ -11,7 +11,7 @@ $ci = $argv[8];
 $path = $argv[8]."/job/Project_CI/ws/".$file;
 
 
-$url = 'http://10.187.39.62:9090/api_select/';
+$url = 'http://10.187.36.162:9090/api_select/';
 $data = array('apiKey' => $apiKey, 'apiSecret' => $apiSecret, 'URL' => $path);
 
 $options = array(
